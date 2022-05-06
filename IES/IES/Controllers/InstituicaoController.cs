0f342de4f3a9957d24a561496b8571a803fbfa40
Daniel Sena <danielsena.csc@outlook.com>
2022-05-06 09:52:07 -0300

    Adiciona os exemplos do capítulo três

---
+++ IES/IES/Controllers/InstituicaoController.cs
@@ -0,0 +1,151 @@
+﻿using IES.Data;
+using IES.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IES.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private readonly IESContext _context;
+
+        public InstituicaoController(IESContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Instituicoes.OrderBy(d => d.Nome).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,Endereco")] Instituicao instituicao)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(instituicao);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError(string.Empty, "Não foi possível inserir os dados");
+            }
+
+            return View(instituicao);
+        }
+
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(d => d.InstituicaoID == id);
+
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituicao);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, [Bind("InstituicaoID,Nome,Endereco")] Instituicao instituicao)
+        {
+            if (id != instituicao.InstituicaoID)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instituicao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstituicaoExists(instituicao.InstituicaoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(instituicao);
+        }
+
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(d => d.InstituicaoID == id);
+
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituicao);
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(d => d.InstituicaoID == id);
+
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituicao);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            var instituicao = await _context.Instituicoes.SingleOrDefaultAsync(i => i.InstituicaoID == id);
+            _context.Instituicoes.Remove(instituicao);
+            TempData["Message"] = "Instituição " + instituicao.Nome.ToUpper() + " foi removida";
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstituicaoExists(long? id)
+        {
+            return _context.Instituicoes.Any(i => i.InstituicaoID == id);
+        }
+    }
+}
